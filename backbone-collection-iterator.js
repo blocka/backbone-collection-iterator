df7145fed3c25a497723357790973a453a8a4839
blocka <atblock@gmail.com>
2013-11-13 04:52:30 -0500

    Don't need the modulus stuff

--- backbone-collection-iterator.js
+++ backbone-collection-iterator.js
@@ -6,12 +6,11 @@
 _.extend(Backbone.Collection.prototype, {
     // return next model or the beginning if at the end
     next: function(model) {
-        return this.at((this.indexOf(model) + 1) % _.size(this));
+        return this.at((this.indexOf(model) + 1));
     },
 
     // return the previous model or the end if at the beginning 
     prev: function(model) {
-        var index = this.indexOf(model) - 1;
-        return this.at(index > -1 ? index : _.size(this) - 1);
+        return this.at((this.indexOf(model) - 1));
     }
 });
